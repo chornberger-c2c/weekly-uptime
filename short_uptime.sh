d0cdf7d97060ac7ce2d8d3de2060f8da8c60c54d
Christopher Hornberger <christopher.hornberger@camptocamp.com>
2023-02-24 16:58:31 +0100

    fix: update short_uptime.sh to suit my wrk

--- short_uptime.sh
+++ short_uptime.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
 # create a nice status line used by screen, by CHO
 
-echo -n $(uptime | awk '{gsub(/,/,"") ; printf  $1 " | up " $3 " " $4 " " ; if ($5 ~ /:/ ) printf  $5; printf " | load "; printf $(NF-2) " " $(NF-1) " " $NF}')
-echo -n " |" $(free -h| awk '/cache:/{print "free " $4}')
-echo -n " |" $(df -h|awk '/root/{print "disk " $5}')
+echo -n $(uptime | awk '{gsub(/,/,"") ; printf  $1 " | up " $3  " " ; if ($5 ~ /:/ ) printf  $5; printf " | load "; printf $(NF-2) " " $(NF-1) " " $NF}')
+echo -n " |" $(free -h| awk '/Mem:/{print "mem free " $7}')
+echo -n " |" $(df -h|awk '/root/{print "root fs " $5}')
+echo  " |" $(df -h|awk '/home/{print "home fs " $5}')
 
-#old rule for uptime, buggy columns were called
-#echo -n $(uptime | awk '{gsub(/,/,"") ; print  $1 " | up " $3 " " $4 " " $5 " | load: " $10 " " $11 " "$12}')
