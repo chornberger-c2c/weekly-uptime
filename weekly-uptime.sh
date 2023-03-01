558b1d53bbe12f9a665a3b7594a1d540fbad39f3
Christopher Hornberger <christopher.hornberger@camptocamp.com>
2023-03-01 10:45:17 +0100

    feat: clean code

--- weekly-uptime.sh
+++ weekly-uptime.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # show this week's machine uptime
 
-# date
-echo -n "$(last | awk '/reboot/ { print $5 " " $6 " " $7 " " $11 }' | head -1)"
+date() {
+  echo -n "$(last | awk '/reboot/ { print $5 " " $6 " " $7 " " $11 }' | head -1)"
+}
 
-# today's hours
-TIME="$(uptime | awk '{ print $3 }' | sed "s/,//")"
-if ! [[  "$TIME" =~ ":" ]]; then
-  echo "(00:$TIME)"
-else
-  echo "(0$TIME)"
-fi  
+hours() {
+  TIME="$(uptime | awk '{ print $3 }' | sed "s/,//")"
+  if ! [[  "$TIME" =~ ":" ]]; then
+    echo "(00:$TIME)"
+  else
+    echo "(0$TIME)"
+  fi
+}
 
-# this week, starting from Monday
-last | awk '/reboot/ { print $5 " " $6 " " $7 " " $11 }' | tail -n +2 | head -5 | grep -B7 Mon
+week() {
+  last | awk '/reboot/ { print $5 " " $6 " " $7 " " $11 }' | tail -n +2 | head -5 | grep -B7 Mon
+}
+
+main() {
+  date 
+  hours 
+  week 
+}
+
+main "$@" || exit 1
