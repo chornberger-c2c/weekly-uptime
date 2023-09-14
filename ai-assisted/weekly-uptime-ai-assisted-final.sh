ad6a628242e0a26d0b20bc7b5812a0cab40b5aba
Christopher Hornberger <christopher.hornberger@camptocamp.com>
2023-09-14 09:08:31 +0200

    fix(today): use uptime without '-p'

--- ai-assisted/weekly-uptime-ai-assisted-final.sh
+++ ai-assisted/weekly-uptime-ai-assisted-final.sh
@@ -13,35 +13,32 @@ current_day=$(date +%a)
 # Loop through each day of the week starting from last Monday (day 1)
 for day in "${days[@]}"; do
 
-    # Calculate uptime for the current day using the last command
-    if [[ "$day" =~ "$current_day" ]]; then
-        uptime_output=$(uptime -p)
-    else
-        uptime_output=$(last -xF | grep -m1 "^reboot.*$day" | awk '{gsub("[()]", "", $16); print $16}')
-    fi
+	# Calculate uptime for the current day using the last command
+	if [[ "$day" =~ "$current_day" ]]; then
+		uptime_output=$(uptime | awk '{print $3}')
+	else
+		uptime_output=$(last -xF | grep -m1 "^reboot.*$day" | awk '{gsub("[()]", "", $16); print $16}')
+	fi
 
-    if [[ "$uptime_output" =~ ([0-9]+):([0-9]+) ]]; then
-        uptime_hours="${BASH_REMATCH[1]}"
-        uptime_minutes="${BASH_REMATCH[2]}"
-    elif [[ "$uptime_output" =~ up\ ([0-9]+)\ hours,\ ([0-9]+)\ minutes ]]; then
-          uptime_hours="${BASH_REMATCH[1]}"
-          uptime_minutes="${BASH_REMATCH[2]}"
-    fi
-    # Remove leading zeros from hours and minutes for calculations
-    uptime_hours=$(echo "$uptime_hours" | sed 's/^0//')
-    uptime_minutes=$(echo "$uptime_minutes" | sed 's/^0//')
-    today_uptime_hours=$(echo "$today_uptime_hours" | sed 's/^0//')
-    today_uptime_minutes=$(echo "$today_uptime_minutes" | sed 's/^0//')
+	if [[ "$uptime_output" =~ ([0-9]+):([0-9]+) ]]; then
+		uptime_hours="${BASH_REMATCH[1]}"
+		uptime_minutes="${BASH_REMATCH[2]}"
+	fi
+	# Remove leading zeros from hours and minutes for calculations
+	uptime_hours=$(echo "$uptime_hours" | sed 's/^0//')
+	uptime_minutes=$(echo "$uptime_minutes" | sed 's/^0//')
+	today_uptime_hours=$(echo "$today_uptime_hours" | sed 's/^0//')
+	today_uptime_minutes=$(echo "$today_uptime_minutes" | sed 's/^0//')
 
-    # Print the uptime for the current day
-    printf "%s Uptime: %02d:%02d\n" "$day" "$uptime_hours" "$uptime_minutes"
-    # Calculate total uptime for the week (excluding future and today)
-    total_hours=$((total_hours + uptime_hours + today_uptime_hours))
-    total_minutes=$((total_minutes + uptime_minutes + today_uptime_minutes))
+	# Print the uptime for the current day
+	printf "%s Uptime: %02d:%02d\n" "$day" "$uptime_hours" "$uptime_minutes"
+	# Calculate total uptime for the week (excluding future and today)
+	total_hours=$((total_hours + uptime_hours + today_uptime_hours))
+	total_minutes=$((total_minutes + uptime_minutes + today_uptime_minutes))
 
-    if [[ "$day" =~ "$current_day" ]]; then
-      break
-    fi
+	if [[ "$day" =~ "$current_day" ]]; then
+		break
+	fi
 done
 
 # Calculate extra hours from excess minutes
@@ -51,4 +48,3 @@ total_minutes=$((total_minutes % 60))
 
 # Print the total uptime for the week so far
 printf "\nTotal Week Uptime: %02d:%02d\n" "$total_hours" "$total_minutes"
-
